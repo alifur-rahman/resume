fd3148b5e32b080cd08a40cc25c8461ffbb35608
Alifur-rahman <70229226+Alifur-rahman@users.noreply.github.com>
2021-01-29 16:07:49 +0600

    gridGallery SlideShow css change
    in this time i was change the gridGallery SlideShow css modify. and fixing mobile menu z-index problems.  adding color changer animation when gridGallery show.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // $('.nav-icon').click(function(){
     // 	$('.header_menu_area ul').slideToggle(1000);
     // 	return false;
     // });
 
-    $('.show-nav').click(function() {
+    $('.show-nav').click(function () {
         $('.responsive-menu').animate({
             'left': '0%',
         }, 500);
@@ -19,7 +19,7 @@ $(document).ready(function() {
     });
 
 
-    $('.close-nav').click(function() {
+    $('.close-nav').click(function () {
         $('.responsive-menu').animate({
             'left': '-100%',
         }, 500);
@@ -34,17 +34,20 @@ $(document).ready(function() {
 
     });
 
-    //slip hover 
-    $(function() {
+    //color changer JS modification          
+    $('.single_portfolio').click(function () {
+        $('.color-changer-area').animate({
+            'top': '30%'
+        }, 1000);
+    });
+    $('.slideshow nav .icon.nav-close').click(function(){
+        $('.color-changer-area').animate({
+            'top': '50%'
+        }, 1000);
+    });
 
-        $('.portfolio_area').sliphover({
-            backgroundColor: 'var(--addColor)',
-            verticalMiddle: false,
-        });
-    })
 
-
-    // $('.sliphover-overlay').addClass('overlay');
+ 
    
 
 
